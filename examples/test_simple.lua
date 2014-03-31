2738ea3d09939df9d14c8e4f5817706a8186a9a2
Andreas Fidjeland <andreas@fidjeland.io>
2014-03-31 13:22:32 +0100

    Fixed whitespace

--- examples/test_simple.lua
+++ examples/test_simple.lua
@@ -5,14 +5,14 @@ require 'totem'
 test = {}
 
 tester = totem.Tester()
- 
+
 function test.A()
     local a = 10
     local b = 10
     tester:asserteq(a, b, 'a == b')
     tester:assertne(a,b,'a ~= b')
 end
- 
+
 function test.B()
     local a = 10
     local b = 9
@@ -22,5 +22,5 @@ end
 function test.C()
     error('Errors are treated differently than failures')
 end
- 
+
 return tester:add(test):run()
