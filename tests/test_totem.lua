178c2053b980a8e3dfa506fcdb4a425c2621abc5
Jack Rae <jwrae@google.com>
2015-08-05 20:53:47 +0100

    checkGradients: accept more general output format.
    Previously child elements of output could not be scalar values and
    output was expected to be of 'array format' e.g. contain contiguous
    integer key values. These constraints are removed.

--- tests/test_totem.lua
+++ tests/test_totem.lua
@@ -180,4 +180,28 @@ function tests.test_TensorArgumentErrorMessages()
 end
 
 
+function tests.test_checkGradientsAcceptsGenericOutput()
+  require 'nn'
+  local Mod = torch.class('totem.dummyClass', 'nn.Module')
+  function Mod:updateOutput(input)
+    local ret = {
+        [1] = {
+            [1] = torch.randn(3, 5),
+            [2] = 1,
+            strKey = 3,
+        },
+        [2] = 1,
+        [3] = torch.randn(3, 5),
+        strKey = 4
+    }
+    return ret
+  end
+  function Mod:updateGradInput(input, gradOutput)
+    return input:clone():fill(0)
+  end
+  local mod = totem.dummyClass()
+  totem.nn.checkGradients(tester, mod, torch.randn(5, 5), 1e-6)
+end
+
+
 tester:add(tests):run()
